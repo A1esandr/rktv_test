d378884920de103d39611a0cc699617363c25131
Alexander Kurakin <A1esandr@users.noreply.github.com>
2015-06-25 00:42:19 +0500

    Update index.php

--- index.php
+++ index.php
@@ -7,24 +7,29 @@
 <body>
   
 <?php 
-
+/* Производим все действия с таблицей только при наличии отправки с формы */
 if(isset($_POST["submit"])) {
 
+/* Выгрузка файла с компьютера пользователя */
   $uploaddir = 'files/';
   $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
   
   if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     echo "Файл корректен и был успешно загружен.\n<br>";
-   
+    
+ /* Если файл успешно выгружен производим действия с таблицей */  
     include 'config.php';
     if(file_exists('./files/'.$_FILES['userfile']['name'])){
-      
+  
+  /* Определяем тип выгруженного файла по трем последним символам в названии */    
       $file_type = substr($_FILES['userfile']['name'], -3,3);
+      
       if($file_type=="xml"){
         $init_xml = simplexml_load_file('./files/'.$_FILES['userfile']['name']);
           
         $link = mysqli_connect($host,$user,$password,$db) or die("Error " . mysqli_error($link));
         
+        /* Переменная для подсчета количества произведенных обновлений или созданий */
         $updated = 0;
         
         for($i=0;$i<count($init_xml);$i++){
@@ -34,19 +39,19 @@ if(isset($_POST["submit"])) {
           $cur_email = $init_xml->user[$i]->email;
           $cur_name = $init_xml->user[$i]->name;
             
-          
+        /* Проверяем наличие пользователя с таким логином в таблице */  
           $query = "SELECT COUNT(*) FROM `users` WHERE users.login = '$cur_login'" or die("Error in the consult.." . mysqli_error($link));
           $result = mysqli_query($link, $query);
           while($row = mysqli_fetch_array($result)) {
             $quant = $row[0];
           }
           if($quant==1) { 
-            
+        /* Если пользователь есть обновляем данные о нем */    
             $query = "UPDATE `users` SET `name`= '$cur_name',`email`= '$cur_email',`updated`= 1 WHERE users.login = '$cur_login'" or die("Error in the consult.." . mysqli_error($link));
             $result = mysqli_query($link, $query);
               
           } else {
-              
+        /* Если нет - создаем такого пользователя */      
             $query = "INSERT INTO users (login, password, name, email, updated) VALUES 
             ('$cur_login', '$cur_password', '$cur_name', '$cur_email', 1)" or die("Error in the consult.." . mysqli_error($link));
             $result = mysqli_query($link, $query); 
@@ -56,7 +61,7 @@ if(isset($_POST["submit"])) {
           $updated++;
         }
       } else if($file_type=="csv") {
-        
+   /* Если файл является csv */     
         $delim = ";";
         $n = 200000;
         
@@ -68,6 +73,7 @@ if(isset($_POST["submit"])) {
           
           while(($csv = fgetcsv($handle,$n,$delim))!==FALSE){
   
+  /* Предполагаем что запись в каждой строке csv файла идет в порядке login;password;name;email; */
             $cur_login = $csv[0];
             $cur_password = $csv[1];
             $cur_name = $csv[2];
@@ -99,21 +105,31 @@ if(isset($_POST["submit"])) {
         die("Загруженный файл не является ни xml, ни csv файлом");
       }
       
+      /*  Удаляем пользователей, которых не было в файле */
       $query = "DELETE FROM `users` WHERE users.updated = 0" or die("Error in the consult.." . mysqli_error($link));
       $result = mysqli_query($link, $query);
+      
+      /* Записываем количество удаленных пользователей */
       $deleted = mysqli_affected_rows($link);
-        
+      
+      
+      /*  Сбрасываем метку обновления - завершение действий с таблицей */  
       $query = "UPDATE `users` SET `updated`= 0" or die("Error in the consult.." . mysqli_error($link));
       $result = mysqli_query($link, $query);
       
       mysqli_close($link);
       
+      /* Общее количество обработанных записей */
       $all = $updated + $deleted;
       
       $message = "Обработано записей: ".$all."\n<br>";
       $message .= "Обновлено записей: ".$updated."\n<br>";
       $message .= "Удалено записей: ".$deleted."\n<br>";
+      
+      /* Отправка отчета на почту */
       mail("example@example.com","Записи",$message);
+      
+      /* Вывод отчета на экран */
       echo $message;
     }
     
