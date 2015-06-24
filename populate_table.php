8a731515f829f8b64bac0dd8daed42dd4246d663
Alexander Kurakin <A1esandr@users.noreply.github.com>
2015-06-25 00:43:29 +0500

    Update populate_table.php

--- populate_table.php
+++ populate_table.php
@@ -9,10 +9,11 @@ for($i=0;$i<count($init_xml);$i++){
   
   $cur_login = $init_xml->user[$i]->login;
   $cur_pass = $init_xml->user[$i]->password;
+  $cur_name = $init_xml->user[$i]->login;
   $cur_email = $cur_login."@example.com";
   
 $query = "INSERT INTO users (login, password, name, email, updated) VALUES 
-('$cur_login', '$cur_pass', '$cur_login', '$cur_email', 0)" or die("Error in the consult.." . mysqli_error($link));
+('$cur_login', '$cur_pass', '$cur_name', '$cur_email', 0)" or die("Error in the consult.." . mysqli_error($link));
 $result = mysqli_query($link, $query);
 
 }
