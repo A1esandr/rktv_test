a650c8d9b15c8de490c43b1a55ad1655d389b1cf
Alexander Kurakin <A1esandr@users.noreply.github.com>
2015-06-24 23:23:17 +0500

    Create populate_table.php

---
+++ populate_table.php
@@ -0,0 +1,22 @@
+<?php
+include 'config.php';
+
+$init_xml = simplexml_load_file('users.xml');
+$link = mysqli_connect($host,$user,$password,$db) or die("Error " . mysqli_error($link));
+
+
+for($i=0;$i<count($init_xml);$i++){
+  
+  $cur_login = $init_xml->user[$i]->login;
+  $cur_pass = $init_xml->user[$i]->password;
+  $cur_email = $cur_login."@example.com";
+  
+$query = "INSERT INTO users (login, password, name, email, updated) VALUES 
+('$cur_login', '$cur_pass', '$cur_login', '$cur_email', 0)" or die("Error in the consult.." . mysqli_error($link));
+$result = mysqli_query($link, $query);
+
+}
+
+echo 'ok';
+mysqli_close($link);
+?>
