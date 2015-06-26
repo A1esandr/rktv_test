60fbb542bd24c7a2570ac1b1e31d2f3c931469cf
Alexander Kurakin <A1esandr@users.noreply.github.com>
2015-06-26 22:22:34 +0500

    Update populate_table.php

--- populate_table.php
+++ populate_table.php
@@ -1,9 +1,11 @@
 <?php
-include 'config.php';
+
+require_once ('MysqliDb.php');
+require_once ('config.php');
 
 $init_xml = simplexml_load_file('users.xml');
-$link = mysqli_connect($host,$user,$password,$db) or die("Error " . mysqli_error($link));
 
+$db = new MysqliDb (DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
 for($i=0;$i<count($init_xml);$i++){
   
@@ -11,13 +13,18 @@ for($i=0;$i<count($init_xml);$i++){
   $cur_pass = $init_xml->user[$i]->password;
   $cur_name = $init_xml->user[$i]->login;
   $cur_email = $cur_login."@example.com";
-  
-$query = "INSERT INTO users (login, password, name, email) VALUES 
-('$cur_login', '$cur_pass', '$cur_name', '$cur_email')" or die("Error in the consult.." . mysqli_error($link));
-$result = mysqli_query($link, $query);
+
+  $data = Array ("login" => "$cur_login",
+                 "password" => "$cur_pass",
+                 "name" => "$cur_name",
+                 "email" => "$cur_email "
+  );
+  $id = $db->insert ('users', $data);
+  if($id)
+    echo 'user was created. Id=' . $id.'<br>';
 
 }
 
 echo 'ok';
-mysqli_close($link);
+
 ?>
